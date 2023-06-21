fbac3f581e25d0d4a180ad096624cabe0a78a597
misterpuffin <snghaoren@gmail.com>
2023-06-21 19:19:05 +0800

    Add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import { defineConfig } from "astro/config";
 import react from '@astrojs/react';
 // import storyblok from "@storyblok/astro";
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    react(),
-    // storyblok({
-    //   accessToken: "<your-access-token>",
-    // }),
-  ],
+  integrations: [react()
+  // storyblok({
+  //   accessToken: "<your-access-token>",
+  // }),
+  , tailwind(), image()],
   vite: {
     ssr: {
-      noExternal: ['antd'],
+      noExternal: ['antd']
     }
   }
 });
